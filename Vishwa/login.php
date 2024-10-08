aec444016989b39602b8be707d23d3451217fe16
bigun-hettiarachchi <it21144844@my.sliit.lk>
2024-10-09 02:53:05 +0530

    update login check of users

--- Vishwa/login.php
+++ Vishwa/login.php
@@ -19,9 +19,36 @@
             <a href="javascript:history.back()" class="back-arrow">&#x2190;</a>
             <h2>Login</h2>
 
-            <form action="../AdminContracts/afterLogin.php" method="post">
+
+            <?php
+
+            require 'config.php';
+
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+                $email = $_POST['email'];
+                $password = $_POST['password'];
+
+                $sql = "SELECT * FROM user WHERE email='$email' AND psw='$password'";
+
+                $result = $con->query($sql);
+
+                if ($result->num_rows > 0) {
+                    echo "<script>alert('User Login Successful');</script>";
+                    echo "<script>window.location.href='../AdminContracts/afterLogin.php';</script>";
+                } else {
+                    echo "<script>alert('Login failed!');</script>";
+                    echo "<script>window.location.href='login.php';</script>";
+                }
+            }
+
+            ?>
+
+
+
+            <form action="../Vishwa/login.php" method="post">
                 <div class="textbox">
-                    <input type="text" placeholder="Username" name="username" required>
+                    <input type="text" placeholder="Email" name="email" required>
                 </div>
 
                 <div class="textbox">
